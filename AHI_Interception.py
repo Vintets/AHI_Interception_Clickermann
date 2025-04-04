29810e0f649c408fddce9a5f8109ea8723a9f7d4
Vintets <vintets@mail.ru>
2025-04-04 16:51:21 +0300

    add function keypress/down/up/+

--- AHI_Interception.py
+++ AHI_Interception.py
@@ -6,6 +6,7 @@ __title__ = '--- AHI_Interception_Clickermann ---'
 __copyright__ = 'Copyright 2024 (c)  bitbucket.org/Vintets'
 
 
+from ctypes import windll, wintypes
 from functools import partial
 import os
 import sys
@@ -45,6 +46,12 @@ MSG_HOOK = {
 
     0xC44B: 'move',
     0xC44C: 'mover',
+
+    0xC450: 'keypress',
+    0xC451: 'keypress_ex',
+    0xC452: 'keydown',
+    0xC454: 'keyup',
+    0xC456: 'keystring',
 }
 
 
@@ -61,9 +68,154 @@ def post_message1(hwnd_self: int) -> None:
 class ICP:
     def __init__(self, debug: bool = False) -> None:
         self.debug = debug
+        self.key_mapping = self._set_key_mapping()
         # interception.capture_keyboard()
         # interception.capture_mouse()
         interception.auto_capture_devices(keyboard=True, mouse=True)
+
+    def _set_key_mapping(self) -> dict[str: int]:  # noqa: CFQ001
+        key_mapping = {
+            'f1': 0x70,
+            'f2': 0x71,
+            'f3': 0x72,
+            'f4': 0x73,
+            'f5': 0x74,
+            'f6': 0x75,
+            'f7': 0x76,
+            'f8': 0x77,
+            'f9': 0x78,
+            'f10': 0x79,
+            'f11': 0x7A,
+            'f12': 0x7B,
+            'f13': 0x7C,
+            'f14': 0x7D,
+            'f15': 0x7E,
+            'f16': 0x7F,
+            'f17': 0x80,
+            'f18': 0x81,
+            'f19': 0x82,
+            'f20': 0x83,
+            'f21': 0x84,
+            'f22': 0x85,
+            'f23': 0x86,
+            'f24': 0x87,
+            'backspace': 0x08,
+            '\b': 0x08,  # same as backspace
+            'tab': 0x09,
+            '\t': 0x09,  # same as tab
+            # 'super': 0x5B,
+            'clear': 0x0C,
+            'enter': 0x0D,
+            '\n': 0x0D,  # same as enter key (newline)
+            'return': 0x0D,
+            'shift': 0x10,
+            'ctrl': 0x11,
+            'alt': 0x12,
+            'pause': 0x13,
+            'capslock': 0x14,
+            'kana': 0x15,
+            'hanguel': 0x15,
+            'hangul': 0x15,
+            'junja': 0x17,
+            'final': 0x18,
+            'hanja': 0x19,
+            'kanji': 0x19,
+            'esc': 0x1B,
+            'escape': 0x1B,
+            'convert': 0x1C,
+            'nonconvert': 0x1D,
+            'accept': 0x1E,
+            'modechange': 0x1F,
+            ' ': 0x20,
+            'space': 0x20,
+            'pgup': 0x21,
+            'pgdn': 0x22,
+            'pageup': 0x21,
+            'pagedown': 0x22,
+            'end': 0x23,
+            'home': 0x24,
+            'left': 0x25,
+            'up': 0x26,
+            'right': 0x27,
+            'down': 0x28,
+            'select': 0x29,
+            'print': 0x2A,
+            'execute': 0x2B,
+            'prtsc': 0x2C,
+            'prtscr': 0x2C,
+            'prntscrn': 0x2C,
+            'printscreen': 0x2C,
+            'insert': 0x2D,
+            'del': 0x2E,
+            'delete': 0x2E,
+            'help': 0x2F,
+            'win': 0x5B,
+            'winleft': 0x5B,
+            'winright': 0x5C,
+            'apps': 0x5D,
+            'sleep': 0x5F,
+            'num0': 0x60,
+            'num1': 0x61,
+            'num2': 0x62,
+            'num3': 0x63,
+            'num4': 0x64,
+            'num5': 0x65,
+            'num6': 0x66,
+            'num7': 0x67,
+            'num8': 0x68,
+            'num9': 0x69,
+            'multiply': 0x6A,
+            '*': 0x6A,
+            'add': 0x6B,
+            'plus': 0x6B,
+            '+': 0x6B,
+            'separator': 0x6C,
+            'subtract': 0x6D,
+            'minus': 0x6D,
+            'dash': 0x6D,
+            'decimal': 0x6E,
+            'divide': 0x6F,
+            'numlock': 0x90,
+            'scrolllock': 0x91,
+            'shiftleft': 0xA0,
+            'shiftright': 0xA1,
+            'ctrlleft': 0xA2,
+            'ctrlright': 0xA3,
+            'altleft': 0xA4,
+            'altright': 0xA5,
+            'browserback': 0xA6,
+            'browserforward': 0xA7,
+            'browserrefresh': 0xA8,
+            'browserstop': 0xA9,
+            'browsersearch': 0xAA,
+            'browserfavorites': 0xAB,
+            'browserhome': 0xAC,
+            'volumemute': 0xAD,
+            'volumedown': 0xAE,
+            'volumeup': 0xAF,
+            'nexttrack': 0xB0,
+            'prevtrack': 0xB1,
+            'stop': 0xB2,
+            'playpause': 0xB3,
+            'launchmail': 0xB4,
+            'launchmediaselect': 0xB5,
+            'launchapp1': 0xB6,
+            'launchapp2': 0xB7,
+        }
+
+        for cod in range(32, 128):
+            key_mapping[chr(cod).lower()] = windll.user32.VkKeyScanA(wintypes.WCHAR(chr(cod)))
+
+        # for k, v in key_mapping.items():
+        #     map_virtual_key = windll.user32.MapVirtualKeyA(divmod(v, 0x100)[1], 0)
+        #     print(f'{k:<19}{v:<5}{map_virtual_key:<3}')
+        return key_mapping
+
+    def get_char_by_keycode(self, keycode) -> Optional[str]:
+        try:
+            return list(self.key_mapping.keys())[list(self.key_mapping.values()).index(keycode)]
+        except ValueError:
+            return None
 
     def move(self, _x: Optional[int] = None, _y: Optional[int] = None) -> None:
         interception.move_to(_x, _y)
@@ -131,12 +283,30 @@ class ICP:
         for _ in range(mult):
             interception.scroll(direction='up')
 
-    def keypress(self, key: str, presses: int = 1, interval: int | float = 0.1, modif_key=None) -> None:
+    def keypress(self, key: str | int, presses: int = 1, interval_ms: int = 0, modif_key=None) -> None:
+        if isinstance(key, int):
+            print(f'lparam {key}')
+            key = self.get_char_by_keycode(key)
+        if key is None:
+            return
+        print(f'{key = }')
+        interval = interval_ms / 1000
         if modif_key is not None:
             with interception.hold_key(modif_key):
                 interception.press(key, presses=presses, interval=interval)
         else:
             interception.press(key, presses=presses, interval=interval)
+
+    def keystring(self, term: str, interval_ms: int = 0) -> None:
+        interval = interval_ms / 1000
+        interception.write(term, interval=interval)
+
+    def keydown(self, key: str, delay: Optional[float | int] = 0) -> None:
+        interception.key_down(key, delay=delay)
+        # UnknownKeyError
+
+    def keyup(self, key: str, delay: Optional[float | int] = 0) -> None:
+        interception.key_up(key, delay=delay)
 
 
 class MainWin(tk.Tk):
@@ -226,6 +396,16 @@ class MainWin(tk.Tk):
                 self.icp.move(*self.unpuck(lparam))
             case 'mover':
                 self.icp.move_relative(*self.unpuck(lparam))
+            case 'keypress':
+                self.icp.keypress(lparam)
+            case 'keypress_ex':
+                self.icp.keypress(lparam, interval_ms=wparam)
+            case 'keydown':
+                self.icp.keydown(lparam)
+            case 'keyup':
+                self.icp.keyup(lparam)
+            # case 'keystring':
+            #     self.icp.keystring()
             case _:
                 if self.debug:
                     print('Неизвестная команда')  # noqa: T201
@@ -283,10 +463,14 @@ if __name__ == '__main__':
     _hight = 50
     if sys.platform == 'win32':
         os.system('color 71')
-        os.system('mode con cols=%d lines=%d' % (_width, _hight))
+        # os.system('mode con cols=%d lines=%d' % (_width, _hight))
 
     authorship(__author__, __title__, __version__, __copyright__)  # width=_width
 
+    # print(interception.KEYBOARD_MAPPING)
+    # print(ICP().key_mapping)
+    # ICP().key_mapping
+    # exit()
     try:
         main(test=False, debug=True)
     except KeyboardInterrupt:
--- Base_Demo_AHI_Interception.py
+++ Base_Demo_AHI_Interception.py
@@ -1,4 +1,5 @@
 import interception
+import time
 
 
 # interception.capture_keyboard()
@@ -12,4 +13,10 @@ interception.move_to(960, 540)
 with interception.hold_key('shift'):
     interception.press('a')
 
-interception.click(120, 160, button='right', delay=1)
+# interception.click(120, 160, button='right', delay=1)
+
+
+interception.click(2480, 420, button='left', delay=1)
+time.sleep(1)
+
+interception.press('up')
